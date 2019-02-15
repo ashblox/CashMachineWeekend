102d40f12940b00873bf412cfe9d55772e89a0d9
Ashley Bloxom <ashley.bloxom@gmail.com>
2019-02-15 16:59:08 -0500

    Added more accounts

--- src/main/java/rocks/zipcode/atm/bank/Bank.java
+++ src/main/java/rocks/zipcode/atm/bank/Bank.java
@@ -20,6 +20,14 @@ public class Bank {
         accounts.put(2000, new PremiumAccount(new AccountData(
                 2000, "Example 2", "example2@gmail.com", 200
         )));
+
+        accounts.put(3000, new PremiumAccount(new AccountData(3000, "Example 3", "example3@gmail.com", 100)
+        ));
+
+        accounts.put(4000, new BasicAccount(new AccountData(4000, "Example 4", "example4@gmail.com", 450)));
+
+        accounts.put(5000, new PremiumAccount(new AccountData(5000, "Example 5" "example5@gmail.com", 300)));
+
     }
 
     public ActionResult<AccountData> getAccountById(int id) {
